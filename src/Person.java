d9a32507487997afc7b91a1955c1a707a6a97765
Nicholas Irawan <nicholas2815@hotmail.com>
2024-12-10 22:50:04 +0700

    Add new Parent Class "Person" that will be extended to all users

---
+++ src/Person.java
@@ -0,0 +1,32 @@
+package tubespbo.src;
+
+import java.util.ArrayList;
+
+public abstract class Person {
+    private String email;
+    private String password;
+    private ArrayList<ChatRoom> chatRooms;
+
+    public Person(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public abstract void callMenuChat();
+}
