5d96ad03bf86463d5645c28b7c78679f8fb168d4
Nicholas Irawan <nicholas2815@hotmail.com>
2024-12-14 22:02:42 +0700

    Updating class 'Tukang' attribute & method except for "confirmOrder()"

--- src/userClass/Tukang.java
+++ src/userClass/Tukang.java
@@ -1,27 +1,40 @@
 package tubespbo.src.userClass;
 
+import tubespbo.src.component.Certificate;
 import tubespbo.src.component.Services;
 
 public class Tukang extends User{
     private double rating;
-    private long[] certification;
+    private Certificate[] certification;
     private double balance;
     private Services serviceCategory;
+    private int orderCount;
+
+    //Dummy purpose only
+    public Tukang(String email, String password, String id, String name, double rating, double balance, Services serviceCategory, int orderCount) {
+        super(email, password, id, name);
+        this.rating = rating;
+        this.balance = balance;
+        this.serviceCategory = serviceCategory;
+        this.orderCount = orderCount;
+    }
+    //------------------
 
     public Tukang(String email, String password, String id, String name, Services serviceCategory) {
         super(email, password, id, name);
         this.rating = 0;
         this.balance = 0;
+        this.orderCount = 0;
         this.serviceCategory = serviceCategory;
         serviceCategory.addListTukang(this);
     }
 
     public double getRating() {
-        return rating;
+        return rating/orderCount;
     }
 
-    public void setRating(double rating) {
-        this.rating = rating;
+    public void addRating(double rating) {
+        this.rating += rating;
     }
 
     public double getBalance() {
@@ -42,5 +55,25 @@ public class Tukang extends User{
         this.serviceCategory.addListTukang(this);
     }
 
+    public void getCertification() {
+        for (Certificate certificate : certification) {
+            System.out.println(certificate.certificateImg);
+        }
+    }
 
+    public void setCertification(int i, Certificate certification) {
+        if (certification.getIdTukang().equals(super.getId()) && certification.isVerified()){
+            this.certification[i] = certification;
+        } else {
+            System.out.println("Sertifikat belum terverifikasi");
+        }
+    }
+
+    public Certificate proposeCertification(long certificationImg) {
+        return new Certificate(super.getId(), certificationImg);
+    }
+
+    public void confirmOrder() {
+        orderCount++;
+    }
 }
