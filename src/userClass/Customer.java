d82cf974717e836abf654e2b3a4de44165a5f85b
Nicholas Irawan <nicholas2815@hotmail.com>
2024-12-14 21:30:35 +0700

    Updating attribute & method for class 'Customer'

--- src/userClass/Customer.java
+++ src/userClass/Customer.java
@@ -1,7 +1,55 @@
 package tubespbo.src.userClass;
 
+import tubespbo.src.component.*;
+
+import java.util.Queue;
+
 public class Customer extends User {
+    private Address[] addresses;
+    private Tukang[] tukangFavorit;
+    private Queue<Order> orderHistory;
+
     public Customer(String email, String password, String id, String name) {
         super(email, password, id, name);
+    }
+
+    public void getAddresses() {
+        for (Address address : addresses) {
+            System.out.println(address);
+        }
+    }
+
+    public void changeAddresses(int i, Address address) {
+        this.addresses[i] = address;
+    }
+
+    public void removeAddress(int i, Address address) {
+        this.addresses[i] = null;
+    }
+
+    public void getTukangFavorit() {
+        for (Tukang tukang : tukangFavorit) {
+            System.out.println(tukang);
+        }
+    }
+
+    public void changeTukangFavorit(int i, Tukang tukang) {
+        if (this.tukangFavorit[i].getId().equals(tukang.getId())) {
+            return;
+        }
+        this.tukangFavorit[i] = tukang;
+    }
+
+    public void getOrderHistory() {
+        for (Order order : orderHistory) {
+            System.out.println(order);
+        }
+    }
+
+    public void addOrderHistory(Order order) {
+        this.orderHistory.add(order);
+        if (this.orderHistory.size() > 10) {
+            this.orderHistory.remove();
+        }
     }
 }
