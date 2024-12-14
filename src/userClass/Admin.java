20d796f19eef311addd7bd5c51b682580082c50e
Nicholas Irawan <nicholas2815@hotmail.com>
2024-12-14 22:03:31 +0700

    Updating attribute to class 'Admin'

--- src/userClass/Admin.java
+++ src/userClass/Admin.java
@@ -1,24 +1,26 @@
 package tubespbo.src.userClass;
 
+import tubespbo.src.component.Certificate;
+
+import java.util.ArrayList;
+
 public class Admin extends Person {
     public Admin(String email, String password) {
         super(email, password);
     }
 
-    public void suspendUser (User user) {
-
+    public void suspendUser (User user, ArrayList<User> users, ArrayList<User> suspendedUsers) {
+        users.remove(user);
+        suspendedUsers.add(user);
     }
 
-    public void activateUser (User user) {
-
+    public void activateUser (User user, ArrayList<User> users, ArrayList<User> suspendedUsers) {
+        suspendedUsers.remove(user);
+        users.add(user);
     }
 
-    public void verifyCertification (Tukang tukang, long certificationImg) {
-
-    }
-
-    public void unverifyCertification (String id) {
-
+    public void certificateVerification (Certificate certification) {
+           certification.setVerified();
     }
 
     public void callMenuChat() {}
